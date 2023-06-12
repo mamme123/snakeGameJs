e195193b870d4327c8d75e3e056dd241b11783bf
M <emohammedd1818@gmail.com>
2023-06-12 12:44:36 +0300

    inserting draw function to draw snake and food including directions

--- js/script.js
+++ js/script.js
@@ -37,3 +37,69 @@ let food = {
   y : (Math.floor(Math.random() *
     rows)) * scale
 }
+// call our draw function every 100 ms
+let playGame = setInterval(draw,100);
+
+//control the snake direction
+// Let's initially make the snake move right
+let d = "right";
+
+
+// Function to draw our snake and the food
+function draw() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	// Draw snake
+	for (let i=0; i<snake.length; i++) {
+		ctx.fillStyle = "#fff";
+		ctx.strokeStyle = "red";
+    ctx.fillRect(snake[i].x,snake[i].y, scale, scale);
+    ctx.strokeRect(snake[i].x,snake[i].y,scale,scale);
+	}
+	// console.log(snake);
+	// Draw food
+	ctx.fillStyle = "#ff0";
+	ctx.strokeStyle = "green";
+	ctx.fillRect(food.x, food.y, scale, scale);
+	ctx.strokeRect(food.x, food.y,scale,scale);
+  // old head position
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+  //console.log(snakeX);
+  // which direction
+  if( d == "left") snakeX -= scale;
+  if( d == "up") snakeY -= scale;
+  if( d == "right") snakeX += scale;
+  if( d == "down") snakeY += scale;
+
+  if (snakeX > canvas.width) {
+    snakeX = 0;
+  }
+  if (snakeY > canvas.height) {
+    snakeY = 0;
+  }
+  if (snakeX < 0) {
+    snakeX = canvas.width;
+  }
+  if (snakeY < 0) {
+    snakeY = canvas.height;
+  }
+  // if the snake eats the food, it grows
+  if(snakeX == food.x && snakeY == food.y){
+      score++;
+      food = {
+          x : (Math.floor(Math.random() * columns)) * scale,
+          y : (Math.floor(Math.random() * rows)) * scale
+      }
+      // we don't remove the tail
+  }else{
+      // remove the tail
+      snake.pop();
+  }
+  console.log(snake);
+  // New head position
+  let newHead = {
+      x : snakeX,
+      y : snakeY
+  }
+	snake.unshift(newHead);
+}
